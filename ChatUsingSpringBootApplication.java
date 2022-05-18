3963f8d80d38de4115db1ea743fd2cfaefd9a83a
Raj Koli <rajkoli8585@gmail.com>
2022-05-18 15:09:54 +0530

    First commit

---
+++ ChatUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.hibenate.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatUsingSpringBootApplication.class, args);
+	}
+
+}
